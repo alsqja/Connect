96f96340f9bc0a3594cafb2f101579679aa843cc
alsqja <alsqha2626@gmail.com>
2025-01-04 16:52:53 +0900

    feat: spring security 설정 추가
    - (UserDetailsImpl) authentication.getPrincipal().getUser() 로 토큰 유저정보 받기 가능
    
    #30

--- src/main/java/com/example/connect/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/connect/domain/user/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.example.connect.domain.user.repository;
 import com.example.connect.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     default User findByIdOrElseThrow(Long id) {
         return findById(id).orElseThrow(() -> new RuntimeException("Not found"));
     }
+
+    Optional<User> findByEmail(String email);
 }
