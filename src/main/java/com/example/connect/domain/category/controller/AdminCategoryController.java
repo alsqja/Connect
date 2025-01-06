aa2cab0ad8425472dc0de0b710f542b2776cdf35
“eunyoung” <jyy714@gmail.com>
2025-01-06 16:46:29 +0900

    feat : 카테고리 등록 기능

---
+++ src/main/java/com/example/connect/domain/category/controller/AdminCategoryController.java
@@ -0,0 +1,31 @@
+package com.example.connect.domain.category.controller;
+
+import com.example.connect.domain.category.dto.AdminCategoryReqDto;
+import com.example.connect.domain.category.dto.AdminCategoryResDto;
+import com.example.connect.domain.category.service.AdminCategoryService;
+import com.example.connect.global.common.dto.CommonResDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/categories")
+@RequiredArgsConstructor
+public class AdminCategoryController {
+
+    private final AdminCategoryService adminCategoryService;
+
+    @PostMapping
+    public ResponseEntity<CommonResDto<AdminCategoryResDto>> createCategory(@Valid @RequestBody AdminCategoryReqDto adminCategoryReqDto) {
+
+        AdminCategoryResDto result = adminCategoryService.createCategory(adminCategoryReqDto);
+
+        return new ResponseEntity<>(new CommonResDto<>("카테고리 생성 완료.", result), HttpStatus.CREATED);
+    }
+}
+
