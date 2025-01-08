c4338e1ce1cbbc1f47690b4f38a1fa228b5e5234
“eunyoung” <jyy714@gmail.com>
2025-01-08 20:35:49 +0900

    feat: 카테고리, 서브 카테고리 전체 조회
    #24

---
+++ src/main/java/com/example/connect/domain/category/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.example.connect.domain.category.controller;
+
+import com.example.connect.domain.category.dto.AllCategoriesResDto;
+import com.example.connect.domain.category.service.CategoryService;
+import com.example.connect.global.common.dto.CommonListResDto;
+import com.example.connect.global.common.dto.CommonResDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/api/categories")
+    public ResponseEntity<CommonListResDto<AllCategoriesResDto>> getAllCategories() {
+
+        List<AllCategoriesResDto> result = categoryService.getAllCategories();
+
+        return new ResponseEntity<>(new CommonListResDto<>("카테고리 및 서브 카테고리 조회 완료.", result), HttpStatus.OK);
+    }
+}
