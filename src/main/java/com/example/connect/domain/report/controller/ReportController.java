5bf5e637e0a4ae753239a0795cb530ec7ad91600
“eunyoung” <jyy714@gmail.com>
2025-01-13 20:01:03 +0900

    feature: 본인 신고 내역 확인 기능
    #49

--- src/main/java/com/example/connect/domain/report/controller/ReportController.java
+++ src/main/java/com/example/connect/domain/report/controller/ReportController.java
@@ -1,5 +1,6 @@
 package com.example.connect.domain.report.controller;
 
+import com.example.connect.domain.report.dto.MyReportResDto;
 import com.example.connect.domain.report.dto.ReportReqDto;
 import com.example.connect.domain.report.dto.ReportResDto;
 import com.example.connect.domain.report.service.ReportService;
@@ -12,10 +13,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -46,5 +49,18 @@ public class ReportController {
         reportService.cancelReport(id, redisUserDto.getId());
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<CommonResDto<MyReportResDto>> getMyReports(@RequestParam(defaultValue = "1") int page,
+                                                                     @RequestParam(defaultValue = "10") int size,
+                                                                     Authentication authentication) {
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        RedisUserDto redisUserDto = userDetails.getUser();
+
+        MyReportResDto results = reportService.getMyReports(page, size, redisUserDto.getId());
+
+        return new ResponseEntity<>(new CommonResDto<>("본인 신고 내역 조회 완료.", results), HttpStatus.OK);
     }
 }
