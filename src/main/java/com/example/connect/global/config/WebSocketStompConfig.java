a6d629fa7ef3a9d789dd8169634f48a5819927be
gyoungsubLee <dltjq2323@gmail.com>
2025-01-16 09:45:37 +0900

    feat: config 설정 변경

--- src/main/java/com/example/connect/global/config/WebSocketStompConfig.java
+++ src/main/java/com/example/connect/global/config/WebSocketStompConfig.java
@@ -14,8 +14,7 @@ public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 
     @Override
