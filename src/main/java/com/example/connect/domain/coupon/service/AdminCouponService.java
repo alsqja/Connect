a77df77e3ea1c4cb5b465c990ee32c0a328369e1
조현지 <qndjddl001@gmail.com>
2025-01-24 15:47:23 +0900

    feat: admin 쿠폰 수정 기능 구현

--- src/main/java/com/example/connect/domain/coupon/service/AdminCouponService.java
+++ src/main/java/com/example/connect/domain/coupon/service/AdminCouponService.java
@@ -1,6 +1,7 @@
 package com.example.connect.domain.coupon.service;
 
 import com.example.connect.domain.coupon.dto.AdminCouponReqDto;
+import com.example.connect.domain.coupon.dto.AdminCouponUpdateReqDto;
 import com.example.connect.domain.coupon.dto.CouponResDto;
 import com.example.connect.domain.coupon.entity.Coupon;
 import com.example.connect.domain.coupon.repository.CouponRepository;
@@ -8,7 +9,9 @@ import com.example.connect.global.error.errorcode.ErrorCode;
 import com.example.connect.global.error.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
@@ -17,12 +20,7 @@ public class AdminCouponService {
     private final CouponRepository couponRepository;
 
     public CouponResDto createCoupon(AdminCouponReqDto adminCouponReqDto) {
-        if (
-                adminCouponReqDto.getExpiredDate().compareTo(adminCouponReqDto.getOpenDate().toLocalDate()) <= 0
-                        || adminCouponReqDto.getOpenDate().isBefore(LocalDateTime.now())
-        ) {
-            throw new BadRequestException(ErrorCode.BAD_REQUEST);
-        }
+        dateCheck(adminCouponReqDto.getOpenDate(), adminCouponReqDto.getExpiredDate());
 
         Coupon coupon = new Coupon(
                 adminCouponReqDto.getName(),
@@ -36,5 +34,42 @@ public class AdminCouponService {
         couponRepository.save(coupon);
 
         return new CouponResDto(coupon);
+    }
+
+    @Transactional
+    public CouponResDto updateCoupon(Long couponId, AdminCouponUpdateReqDto adminCouponUpdateReqDto) {
+        Coupon coupon = couponRepository.findByIdOrElseThrow(couponId);
+
+        if (adminCouponUpdateReqDto.getExpiredDate() != null && adminCouponUpdateReqDto.getOpenDate() != null) {
+            dateCheck(adminCouponUpdateReqDto.getOpenDate(), adminCouponUpdateReqDto.getExpiredDate());
+        }
+
+        if (adminCouponUpdateReqDto.getOpenDate() != null && adminCouponUpdateReqDto.getExpiredDate() == null) {
+            dateCheck(adminCouponUpdateReqDto.getOpenDate(), coupon.getExpiredDate());
+        }
+
+        if (adminCouponUpdateReqDto.getOpenDate() == null && adminCouponUpdateReqDto.getExpiredDate() != null) {
+            dateCheck(coupon.getOpenDate(), adminCouponUpdateReqDto.getExpiredDate());
+        }
+
+        coupon.updateCoupon(
+                adminCouponUpdateReqDto.getName(),
+                adminCouponUpdateReqDto.getDescription(),
+                adminCouponUpdateReqDto.getCount(),
+                adminCouponUpdateReqDto.getAmount(),
+                adminCouponUpdateReqDto.getExpiredDate(),
+                adminCouponUpdateReqDto.getOpenDate(),
+                adminCouponUpdateReqDto.getIsDeleted()
+        );
+
+        CouponResDto couponResDto = new CouponResDto(coupon);
+
+        return couponResDto;
+    }
+
+    private void dateCheck(LocalDateTime openDate, LocalDate expiredDate) {
+        if (expiredDate.compareTo(openDate.toLocalDate()) <= 0 || openDate.isBefore(LocalDateTime.now())) {
+            throw new BadRequestException(ErrorCode.BAD_REQUEST);
+        }
     }
 }
