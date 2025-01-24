09e856b87c62a4f15ae651d651da8c03df8b6b5e
alsqja <alsqha2626@gmail.com>
2025-01-24 09:58:38 +0900

    feat: 피드 수정
    #158

--- src/main/java/com/example/connect/domain/userimage/service/UserImageService.java
+++ src/main/java/com/example/connect/domain/userimage/service/UserImageService.java
@@ -6,12 +6,16 @@ import com.example.connect.domain.userimage.dto.UserImagePageResDto;
 import com.example.connect.domain.userimage.dto.UserImageResDto;
 import com.example.connect.domain.userimage.entity.UserImage;
 import com.example.connect.domain.userimage.repository.UserImageRepository;
+import com.example.connect.global.error.errorcode.ErrorCode;
+import com.example.connect.global.error.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -37,5 +41,19 @@ public class UserImageService {
         Page<UserImage> userImagePage = userImageRepository.findByUserId(userId, pageable);
 
         return new UserImagePageResDto(userImagePage);
+    }
+
+    @Transactional
+    public UserImageResDto updateImage(Long userId, Long id, String url, String description) {
+
+        Optional<UserImage> userImage = userImageRepository.findByUserIdAndId(userId, id);
+
+        if (userImage.isEmpty()) {
+            throw new NotFoundException(ErrorCode.NOT_FOUND);
+        }
+
+        userImage.get().updateFields(url, description);
+        
+        return new UserImageResDto(userImage.get());
     }
 }
