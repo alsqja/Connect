49a8b010092c0b91d7438457fec1f067b8b124fd
gyoungsubLee <dltjq2323@gmail.com>
2025-01-31 19:45:02 +0900

    fix: 채팅방 삭제 오류 수정

--- src/main/java/com/example/connect/domain/chat/entity/Chat.java
+++ src/main/java/com/example/connect/domain/chat/entity/Chat.java
@@ -17,7 +17,7 @@ public class Chat {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_chatroom_id", nullable = false)
+    @JoinColumn(name = "user_chatroom_id", nullable = true)
     private UserChatroom userChatroom;
 
     @Column(name = "chatroom_id", nullable = false)
