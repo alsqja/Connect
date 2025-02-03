e2dc6b969915c4cd761cb57aecedc737318645fd
alsqja <alsqha2626@gmail.com>
2025-02-03 22:19:23 +0900

    feat: 기본 어드민 유저, 가입 축하 쿠폰 PostConstructor 추가

--- src/main/java/com/example/connect/global/config/DeployDataInitializer.java
+++ src/main/java/com/example/connect/global/config/DeployDataInitializer.java
@@ -1,4 +1,35 @@
 package com.example.connect.global.config;
 
+import com.example.connect.domain.coupon.entity.Coupon;
+import com.example.connect.domain.coupon.repository.CouponRepository;
+import com.example.connect.domain.user.entity.User;
+import com.example.connect.domain.user.repository.UserRepository;
+import com.example.connect.global.enums.Gender;
+import com.example.connect.global.enums.UserRole;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Profile("prod")
+@Component
+@RequiredArgsConstructor
 public class DeployDataInitializer {
+
+    private final UserRepository userRepository;
+    private final CouponRepository couponRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        User user = new User("admin@gmail.com", passwordEncoder.encode("Password1!"), "관리자", "10000814", Gender.MAN, "https://ca.slack-edge.com/T06B9PCLY1E-U07KRNHKXUM-4ddfb9e4780d-512", false, UserRole.ADMIN);
+        userRepository.save(user);
+
+        Coupon coupon = new Coupon("가입 축하 쿠폰", "가입을 축하드립니다! (매칭 무료 5회 쿠폰)", 0, 5, LocalDate.MAX, LocalDateTime.now());
+        couponRepository.save(coupon);
+    }
 }
